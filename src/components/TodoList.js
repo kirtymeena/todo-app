5ad21f1e4163f539ada7d361151ce4addabe210b
Kirty Meena <kirtymeena@gmail.com>
2022-04-17 19:58:11 +0530

    fixed warning

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteTodo, editTodo, toggleComplete } from "../redux/TodoListSlice";
 import { MdModeEdit } from "react-icons/md";
