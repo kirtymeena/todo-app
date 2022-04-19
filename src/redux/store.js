e41d6407effcc7bc1ec543b33dd56c1e0f6ea213
Kirty Meena <kirtymeena@gmail.com>
2022-04-19 13:44:55 +0530

    added redux persist to save the states

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./themeSlice";
 import TodoListReducer from "./TodoListSlice";
 import { combineReducers } from "redux";
